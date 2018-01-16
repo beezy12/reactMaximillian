ef737c763b4ac2a9e88cd8f885aa2dcd0d3de0de
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-01-15 20:03:37 -0600

    refactor and map users into components

--- src/App.js
+++ src/App.js
@@ -47,31 +47,30 @@ class App extends Component {
           cursor: 'pointer'
       };
 
+      let persons = null;
+
+      if (this.state.showPersons) {
+          persons = (
+              <div>
+                  {this.state.persons.map(person => {
+                      return <Person
+                          name={person.name}
+                          age={person.age} />
+                  })}
+              </div>
+          );
+      }
+
 
       return (
           <div className="App">
-          <h1>I AM REACT</h1>
-          <button
-              style={style}
-              onClick={this.togglePersonsHandler}>Switch name</button>
-          {
-              this.state.showPersons === true ?
-                <div>
-                    <Person
-                      name={this.state.persons[0].name}
-                      age={this.state.persons[0].age}/>
-                    <Person
-                      name={this.state.persons[1].name}
-                      age={this.state.persons[1].age}
-                      click={this.switchNameHandler.bind(this, 'toonces')}
-                      changed={this.nameChangeHandler} >My hobbies: being a good boy</Person>
-                    <Person
-                      name={this.state.persons[2].name}
-                      age={this.state.persons[2].age}/>
-                </div> : null
-            }
-        </div>
-    );
+              <h1>I AM REACT</h1>
+              <button
+                  style={style}
+                  onClick={this.togglePersonsHandler}>Show Peoples</button>
+              {persons}
+          </div>
+      );
   }
 }
 
