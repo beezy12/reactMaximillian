f74437b455569c8cac91aeb9782ae5330ccff6c1
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-01-16 22:38:50 -0600

    flexible refactor. spread operators

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            { name: 'beezy', age: 38 },
-            { name: 'bubby', age: 10 },
-            { name: 'manna', age: 29 }
+            { id: 'oouo', name: 'beezy', age: 38 },
+            { id: 'ieieuie', name: 'bubby', age: 10 },
+            { id: 'oeieo', name: 'manna', age: 29 }
         ],
         showPersons: false
     }
 
-  nameChangeHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'briguy', age: 38 },
-        { name: event.target.value, age: 11 },
-        { name: 'manna', age: 2 }
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+        return p.id === id;
+    });
+
+    // using spread operator here to avoid just using a reference to the state object. you NEVER want to mutate state directly...and you'd only be mutating a reference to the state object. so use spread operator to make a COPY of the state object
+    const person = {
+        ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    // ALWAYES WORK WITH COPIES
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    // **** this is another way to avoid mutating references to state objects. you can just create a new object
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    this.setState( {persons: persons} )
   }
 
   togglePersonsHandler = () => {
@@ -56,7 +69,9 @@ class App extends Component {
                       return <Person
                           name={person.name}
                           age={person.age}
-                          click={() => this.deletePersonHandler(index)} />
+                          click={() => this.deletePersonHandler(index)}
+                          key={person.id}
+                          changed={(event) => this.nameChangeHandler(event, person.id)} />
                   })}
               </div>
           );
